c6fc78022320e8858b5a944d3ff5ec04666979a1
Bouke Haarsma <bouke@haarsma.eu>
2018-10-19 08:03:50 +0200

    Added tests 4-7 to Linux test suite

--- Tests/HKDFTests/HKDFTests.swift
+++ Tests/HKDFTests/HKDFTests.swift
@@ -9,6 +9,10 @@ class HKDFTests: XCTestCase {
             ("test1", test1),
             ("test2", test2),
             ("test3", test3),
+            ("test4", test4),
+            ("test5", test5),
+            ("test6", test6),
+            ("test7", test7),
             ("testLinuxTestSuiteIncludesAllTests", testLinuxTestSuiteIncludesAllTests)
         ]
     }
